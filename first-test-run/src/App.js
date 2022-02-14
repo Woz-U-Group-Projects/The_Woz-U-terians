91dd609c14091404a25a5d16ea66ec3ea1428af5
Francisco Andrade <88457714+paco-801@users.noreply.github.com>
2022-02-14 14:42:25 -0700

    updated inventory page with unordered list

--- first-test-run/src/App.js
+++ first-test-run/src/App.js
@@ -1,31 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Inventory from './Inventory';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          It's Alive! :D
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/Woz-U-Group-Projects/The_Woz-U-terians"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link to GitHub Project
-        </a>
-        <a
-          className="App-link"
-          href="https://trello.com/b/YPCc93fo/woz-u-terrians-project"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link to Trello Board
-        </a>
-      </header>
+      <Inventory />
     </div>
   );
 }
---
+++ first-test-run/src/Inventory.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Inventory(){
+    return(
+        <div>
+            <h1>Welcome to the Inventory Page! :D</h1>
+            <h2>Info this page should display</h2>
+            <ul>
+                <li>Product Make(r)</li>
+                <li>Product Model(Phone Name)</li>
+                <li>Storage Capacity</li>
+                <li>Color Options</li>
+                <li>(Optional)Units sold in last 90/60/30 days</li>
+
+                <h4>From example found Online</h4>
+                
+                <li>Supply Plan</li>
+                <li>Demand Plan</li>
+                <li>Planned Inventory</li>
+                <li>On Order</li>
+                <li>In Transit</li>
+                <li>Sales Orders</li>
+                <li>On Hand Inventory</li>
+                
+            </ul>
+        </div>
+    );
+}
+
+export default Inventory;
