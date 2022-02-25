6d7ea4d254e2c67d80b79bde298e89d59a56ef6d
paco-801 <paco_8489@yahoo.com>
2022-02-24 17:32:00 -0700

    Home page displays hello world
    Its alive! :D Now add links and shortcuts to homepage to route to other pages.

--- first-test-run/src/App.js
+++ first-test-run/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home';
 import Test from './components/Test';
-import { Route, Router, Link, Routes } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
     return (
-        <div className='App'>
-            <Route exact path='/' component={Home} />
-        </div>
+        <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="test" element={<Test />} />
+      </Routes>
+    </div>
     );
 }
 
