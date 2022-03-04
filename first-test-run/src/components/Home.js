b0895056031e33abc1a02a11bb285ee9fe01fc64
Francisco Andrade <88457714+paco-801@users.noreply.github.com>
2022-03-03 21:10:46 -0700

    cleaned up navbar and added logo

--- first-test-run/src/components/Home.js
+++ first-test-run/src/components/Home.js
@@ -20,22 +20,7 @@ function Home() {
       <li class="nav-item">
         <a class="nav-link" href="/inventoryTable">Inventory Overview</a>
       </li>
-      {/* <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li> */}
-    </ul>
-    {/* <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form> */}
+      </ul>
     <div className='titleText'>Francisco's Inventory Tracker | FINAL PROJECT</div>
   </div>
 </nav>
--- first-test-run/src/components/InventoryTable.js
+++ first-test-run/src/components/InventoryTable.js
@@ -4,43 +4,25 @@ function InventoryTable() {
   return (
     <>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-          Site Shortcuts
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="/">
-                Home <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/test">
-                Test Page
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/inventoryTable">
-                Inventory Overview
-              </a>
-            </li>
-          </ul>
-          <div className="titleText">
-            Woz-U Terrians Inventory Tracker | FINAL PROJECT
-          </div>
-        </div>
-      </nav>
+  <a class="navbar-brand" href="#">Site Shortcuts</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/test">Test Page</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/inventoryTable">Inventory Overview</a>
+      </li>
+      </ul>
+    <div className='titleText'>Francisco's Inventory Tracker | FINAL PROJECT</div>
+  </div>
+</nav>
       <div>InventoryTable</div>
     </>
   );
